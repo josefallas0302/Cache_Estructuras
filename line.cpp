5bac627d5290dc5e9242c9b52a529d5c0dde20b5
juanjosedq <juanjose.dq@gmail.com>
2017-04-13 00:56:42 -0600

    line.cpp

--- line.cpp
+++ line.cpp
@@ -22,7 +22,36 @@ class Line {
 		/*set_block (int n , block) {
 			n_block
 		}*/
-		
+	
+
+
+	bool compare(int tag_R, int block){              //FIXME
+		int tag_B = n_blocks[block].get_tag();
+		if (tag_R == tag_B){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+
+	void write(int data_R, int tag_R){
+		char died = n_blocks[0].get_died();
+		if(died == 'V'){
+			n_blocks[0].set_data(data_R);
+			n_blocks[0].set_tag(tag_R);
+			n_blocks[0].set_died('N');
+			n_blocks[1].set_died('V');
+		}else{
+			n_blocks[1].set_data(data_R);
+			n_blocks[1].set_tag(tag_R);
+			n_blocks[1].set_died('N');
+			n_blocks[0].set_died('V');
+		}
+	}
+
+	
+
 };
 
 int main(){
