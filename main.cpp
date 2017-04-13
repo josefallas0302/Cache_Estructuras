a2979ad7f0deac5de33b38e481e23bb9b49c92ab
juanjosedq <juanjose.dq@gmail.com>
2017-04-13 02:06:53 -0600

    cambios

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ using namespace std;
 
 int main (){
 
-  int datos[32]={31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-  Block newBlock = Block(0, 'i', 0, datos, 0);
+  int datos[32]={31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  Block newBlock = Block(0, 'i', 0, datos, 'N');
   int a = newBlock.get_tag();
   int b = newBlock.get_valid();
   char c = newBlock.get_state();
-  int d = newBlock.get_died();
-  cout << "valid = " << b <<  "state = " << c <<  "died = " << d <<  "tag = " << a << endl;
-  newBlock.set_tag(15);
-  int a = newBlock.get_tag();
-  cout << a << endl;
+  char d = newBlock.get_died();
+  cout << "valid = " << b <<  " state = " << c <<  " died = " << d <<  " tag = " << a << endl;
+  for (int i=0; i<32; i++){
+  cout << newBlock.get_data(i) << endl;
+  }
+
   return 0;
   
 };
