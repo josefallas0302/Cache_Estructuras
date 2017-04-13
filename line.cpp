f089e5c3fc109825c7ce7dca0e930ba367415ec8
jose <ariel@talista.arcoslab.eie.loc>
2017-04-13 01:04:02 -0600

    mierda

--- line.cpp
+++ line.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Line {
 	private:
 		int n_sets;
-                list <Block> n_blocks;		
+                vector <Block> n_blocks;		
 	public:
 // Constructor, toma n_bloques por set y genera una lista con bloques invalidos
 
@@ -18,18 +18,25 @@ class Line {
        }
   	return;
   	}
+// Devuelve el bloque
+		Block get_block (int n) {
+			Block Block_n
+			return Block_n
+		}
+
 		
-// Carga un bloque al set
-		/*set_block (int n , block) {
-			n_block
-		}*/
+// Carga un bloque al set				
+		void set_block (int n, Block newBlock) {
+			n_blocks[n] = newBlock;
+		}
 		
 };
 
 int main(){
-  Line m = Line(2);
+ /* Line m = Line(2);
   Block a = m.n_blocks.begin();
   int b = a.get_tag();
-  cout << b << endl;
+  cout << b << endl;*/
+	return 0;
 };
->>>>>>> 82357892b44bc180bd75004c978f4f0e41ccccd4
+
