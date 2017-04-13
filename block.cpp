54b79e9e38a8c08e9badf2d5c8d9a29b001e3ab1
juanjosedq <juanjose.dq@gmail.com>
2017-04-12 20:50:42 -0600

    CRACK

--- block.cpp
+++ block.cpp
@@ -1,8 +1,26 @@
-#include "block.h"
 
+class Block {
+	private:
 
-Block :: Block(int valid, char state, int tag, int data) : B_valid(valid), B_state(state), B_tag(tag), B_data(data){
+		int B_valid;
+		char B_state;
+		int B_tag;
+		int B_data;
+
+	public:
+
+		//Default constructor (Default Ctor)
+		Block(int valid, char state, int tag, int data) : B_valid(valid), B_state(state), B_tag(tag), B_data(data){
 			return;
 		}
 
+		int get_tag (void){
+		  return B_tag;
+		}
+
+		void set_tag (int new_tag){
+		  B_tag = new_tag;
+		}
+
+};
 
