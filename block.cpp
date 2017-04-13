25d37b7829ec7cd397b109e9423df6bd925aae9e
juanjosedq <juanjose.dq@gmail.com>
2017-04-12 18:40:07 -0600

    ordenando .h .cpp

--- block.cpp
+++ block.cpp
@@ -1,34 +1,8 @@
-#include <stdio.h>
-#include <iostream> 
-using namespace std;
+#include "block.h"
 
 
-class Block {
-	private:
-		int valid;
-		int modified;
-		int exclusive;
-		int shared;
-		int invalid;
-		int tag;
-		int data;
-	public:
-		void SetSize (int size) {
-			c_size = size;
+Block :: Block(int valid, char state, int tag, int data) : B_valid(valid), B_state(state), B_tag(tag), B_data(data){
+			return;
 		}
-		void SetType (int type) {
-			c_type = type;
-		}
-		void SetBlock (int block){
-			c_block_size = block;
-		}
-		void GetState (char state){
-			c_state = state;
-		}
-		
-	/*	cargar_dato(int set, ):
-			for(i=0,i<)		
-*/
 
 
-};
---
+++ block.h
@@ -0,0 +1,18 @@
+int main(){
+  return 0;
+};
+
+class Block {
+	private:
+
+		int B_valid;
+		char B_state;
+		int B_tag;
+		int B_data;
+
+	public:
+
+		//Default constructor (Default Ctor)
+		Block (int valid, char state, int tag, int data);
+
+};
